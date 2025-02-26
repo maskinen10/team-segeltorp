74adc8886812159397ed4d3c5a857f0bc3d41789
maskinen10 <fransmannen10@gmail.com>
2025-02-26 17:34:11 +0100

    Create brands.ts

---
+++ constants/brands.ts
@@ -0,0 +1,35 @@
+```typescript
+export const NEW_CAR_BRANDS = [
+  'RENAULT',
+  'DACIA',
+  'ALPINE',
+  'SUBARU',
+  'SMART',
+  'BYD',
+  'HONGQI',
+  'JEEP',
+  'ALFA ROMEO',
+  'ABARTH',
+  'FIAT',
+  'RAM',
+  'DODGE',
+  'INEOS',
+] as const;
+
+export const BRAND_COLORS = {
+  RENAULT: '#FFC300',
+  DACIA: '#2B4C9B',
+  ALPINE: '#0046FF',
+  SUBARU: '#013C74',
+  SMART: '#FF0000',
+  BYD: '#FF0000',
+  HONGQI: '#BC0019',
+  JEEP: '#4A4A4A',
+  'ALFA ROMEO': '#C9202E',
+  ABARTH: '#D00A0A',
+  FIAT: '#223D77',
+  RAM: '#000000',
+  DODGE: '#C9202E',
+  INEOS: '#2C2C2C',
+};
+```
